05c9ad356fc9ea6172cc76611f39445d269e92f3
Patrick Wegerer <patrick.wegerer@gmail.com>
2018-10-19 11:47:31 +0200

    Added NPM package with a succesful try

--- convert.js
+++ convert.js
@@ -1,1 +1,13 @@
-console.log('Not implemented yet!');
+let num = 10;
+let unit1 = "m";
+const to = "to";
+let unit2 = "mm";
+
+var convert = require('convert-units');
+
+if(process.argv.length<6){
+    
+}
+
+console.log(convert(num).from(unit1).to(unit2));
+
